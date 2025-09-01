03d593fa18e330cfba7b6321405f0dc1884a9b18
Mays Alreem <hroubmays@gmail.com>
2025-09-01 16:57:18 +0300

    test(auth): add unit tests for GetAPIKey function

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAPIKey_Valid(t *testing.T) {
+	header := http.Header{}
+	apikey := "dummy-api-key"
+	headerKey := "Authorization"
+	headerVal := fmt.Sprintf("ApiKey %s", apikey)
+	header.Add(headerKey, headerVal)
+
+	returnedKey, err := GetAPIKey(header)
+	require.NoError(t, err)
+	assert.Equal(t, apikey, returnedKey)
+}
+
+func TestGetAPIKey_Invalid_EmptyAuthHeader(t *testing.T) {
+	header := http.Header{}
+	
+	returnedKey, err := GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, "", returnedKey)
+}
+
+func TestGetAPIKey_Invalid_HeaderLengthIsNotTwo(t *testing.T) {
+	header := http.Header{}
+	apikey := "dummy-api-key"
+	headerKey := "Authorization"
+	header.Add(headerKey, apikey)
+
+	returnedKey, err := GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, "", returnedKey)
+}
+
+func TestGetAPIKey_Invalid_NoApiKeyWordInHeaderValue(t *testing.T) {
+	header := http.Header{}
+	apikey := "dummy-api-key"
+	headerKey := "Authorization"
+	headerVal := fmt.Sprintf("NotApiKey %s", apikey)
+	header.Add(headerKey, headerVal)
+
+	returnedKey, err := GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, "", returnedKey)
+}
